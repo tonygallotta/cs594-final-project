84f03380c05fecd2b62d97cfbc2bbe4abcbf7049
Tony Gallotta <anthony.gallotta@gmail.com>
2015-03-26 17:23:47 +0000

    allowing port configuration

--- webapp/app.js
+++ webapp/app.js
@@ -9,10 +9,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+var port = process.env.PORT || 3000
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.set('port', port);
+
+console.log("Using port", port);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
