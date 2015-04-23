9e67014423296c812e3093782718cc64ae3f89e4
Tony Gallotta <tgallotta@signal.co>
2015-04-23 08:09:32 -0500

    adding ffmpeg info

--- yt_example.py
+++ yt_example.py
@@ -1,8 +1,13 @@
 import yt
 
-prefix = "http://darksky.slac.stanford.edu/scivis2015/data/ds14_scivis_0128/ds14_scivis_0128_e4_dt04_1.0000"
-file = "data/ds14_scivis_0128_e4_dt04_0.9000"
+file = "http://darksky.slac.stanford.edu/scivis2015/data/ds14_scivis_0128/ds14_scivis_0128_e4_dt04_1.0000"
 ds = yt.load(file)
 ad = ds.all_data()
 p = yt.ProjectionPlot(ds, 0, 'dark_matter_density', weight_field=None)
-p.save()
+p.set_cmap(field="dark_matter_density", cmap='Beach_r')
+plot = p.plots['deposit', 'dark_matter_density']
+plot.hide_axes()
+plot.hide_colorbar()
+p.save("beach.png")
+p.set_cmap(field="dark_matter_density", cmap='BLUE')
+p.save("green.png")
